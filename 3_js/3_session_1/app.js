f0552271bd3a0c994d7a9809f79e1b43db2718aa
owlab-io <contact@owlab.io>
2017-10-23 11:17:26 +0200

    correction config / créer repo git

--- 3_js/3_session_1/app.js
+++ 3_js/3_session_1/app.js
@@ -4,21 +4,25 @@
         alert(p);
     }
 
-    // afficherMessage("Hello");
-
     function ecrireSurUneLigne(a, b){
         console.log(a + " " + b);
     }
 
     function multiplier(a, b) {
-        return a * b;
+        var res = a * b, error = isNaN(res);
+        if (error) {
+            return alert("erreur de multiplication !");
+        }
+        return res;
     }
-
-    ecrireSurUneLigne("c'est l'heure", "d'aller manger");
 
     window.onload = function() {
         var div = document.getElementById("ma_premiere_selection");
+        // ecrireSurUneLigne("c'est l'heure", "d'aller manger");
+        // afficherMessage("Hello");
         console.log("le node html sélectionné ....");
+        console.log(multiplier("toto", "titi"));
+        console.log(multiplier("toto", 23));
         console.log(div);
         div.textContent = multiplier(103.66, 10032.45);
     };

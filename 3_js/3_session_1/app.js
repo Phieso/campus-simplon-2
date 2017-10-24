771b134ae20ac99c44aae4511267b1d6fb86e825
owlab-io <contact@owlab.io>
2017-10-24 09:28:59 +0200

    ajout TP du 24/10/17

--- 3_js/3_session_1/app.js
+++ 3_js/3_session_1/app.js
@@ -1,5 +1,21 @@
 (function session1JS() {
 
+    var user1, user2;
+
+    user1 = {
+        name: "Charlène",
+        age: 33
+    };
+
+    user2 = {
+        name: "Yanis",
+        age: 25
+    };
+
+    user3 = {
+        hobbies: ["escalade", "musique"]
+    };
+
     function afficherMessage(p) {
         alert(p);
     }
@@ -8,23 +24,56 @@
         console.log(a + " " + b);
     }
 
+    var isNumber = function isNumber(value) {
+       return typeof value === 'number' || typeof value === 'boolean' && !isNaN(value);
+    }
+
     function multiplier(a, b) {
-        var res = a * b, error = isNaN(res);
-        if (error) {
-            return alert("erreur de multiplication !");
+        var res;
+
+        if (isNumber(a) && isNumber(b)) {
+            res = a * b;
+
+        } else if (!a || !b) {
+           res = "operandes manquants";
+
+       } else {
+           // au moins un des deux n'est pas un nombre ....
+           res = "erreur de multiplication";
         }
+
         return res;
+    }
+
+    function getAge(user) {
+        return user.age || " - âge non renseigné -";
+    }
+
+    function getName(user) {
+        return user.name || "anonyme";
+    }
+
+    function displayUserInfo(user) {
+        console.log(`L'utilisateur se nommant ${getName(user)} est âgé(e) de ${getAge(user)} ans`);
     }
 
     window.onload = function() {
         var div = document.getElementById("ma_premiere_selection");
+        // div.innerHTML = "<p><span></span></p>";
+        console.log("le node html sélectionné ....");
+        console.log(div);
+        console.log(div.tagName);
+        console.log(div.id);
+        console.log(div.children[0]);
+        if (div.children.length >= 1) {
+            div.style.background = "red";
+        } else {
+            div.style.background = "green";
+        }
         // ecrireSurUneLigne("c'est l'heure", "d'aller manger");
         // afficherMessage("Hello");
-        console.log("le node html sélectionné ....");
-        console.log(multiplier("toto", "titi"));
-        console.log(multiplier("toto", 23));
-        console.log(div);
-        div.textContent = multiplier(103.66, 10032.45);
+        // div.textContent = multiplier(false, 333);
+        displayUserInfo(user3);
     };
 
 
---
+++ 3_js/8_entrainement_4/app.js
@@ -0,0 +1,18 @@
+(function entrainement4() {
+
+    var header, nav, sidebar, main, footer;
+
+    window.onload = function() {
+        console.log("document ready !");
+        console.log("--- header ---");
+        console.log(header);
+        console.log("--- nav ---");
+        console.log(nav);
+        console.log("--- sidebar ---");
+        console.log(sidebar);
+        console.log("--- main ---");
+        console.log(main);
+        console.log("--- footer ---"); 
+        console.log(footer);
+    }
+}());

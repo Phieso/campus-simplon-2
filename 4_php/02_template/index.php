75437d4a0ee30e71a810b6593744f127464c1ddd
owlab-io <contact@owlab.io>
2017-12-08 07:34:28 +0100

    améliorations php/template

--- 4_php/02_template/index.php
+++ 4_php/02_template/index.php
@@ -4,14 +4,17 @@
     <h1 class="title"><?php echo $config->title_home ?></h1>
     <p class="intro">
         <b>Intro :</b> Modifier le CSS pour rendre le header fixe.<br><br>
-        <b>TP Burger :</b> coder un plugin burger (HTML/CSS/JS) et l'intégrer à votre copie du template.<br><br> Un click sur l'icône burger toggle une navigation secondaire. L'icône burger à un état actif et non-actif.<br><br> Utiliser une transition CSS pour afficher/masquer l'élément. La nav contient des liens vers les pages musique et profil.<br><br> Au click, un sous programme js s'occupe d'empêcher le comporterment par défaut.<br><br>
+        <b>TP Burger :</b> coder deux modules navbar et burger (HTML/CSS/JS). Les intégrer à votre copie du template.<br><br> Un click sur l'icône burger toggle une navigation secondaire. L'icône burger à un état actif et non-actif (JS/CSS).<br><br> Utiliser une transition CSS pour afficher/masquer l'élément. La nav contient des liens vers les pages musique et profil.<br><br> Au click, un sous programme JS s'occupe d'empêcher le comporterment par défaut.<br><br>
         <b>PS :</b> nous chargeront ces pages en AJAX.<br>
     </p>
-    <b>structure du module (@modules/burger):</b>
+    <b>structure des modules :</b>
     <ul>
-        <li>burger.html</li>
-        <li>burger.js</li>
-        <li>burger.css</li>
+        <li>modules/burger/burger.html</li>
+        <li>modules/burger/burger.js</li>
+        <li>modules/burger/burger.css</li>
+        <li>modules/navbar/nav.php</li>
+        <li>modules/navbar/nav.js</li>
+        <li>modules/navbar/nav.css</li>
     </ul>
     <p class="intro">
         TP ajaxPageLoad: Coming soon !

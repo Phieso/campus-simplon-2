089b7703270070b06c14677b162da7c66ef7a9ee
owlab-io <contact@owlab.io>
2017-12-08 07:37:46 +0100

    ajout du tp contact form

--- 4_php/02_template/contact.php
+++ 4_php/02_template/contact.php
@@ -5,7 +5,8 @@
 <div id="app" class="page">
     <h1 class="title"><?php echo $config->title_contact ?></h1>
     <p class="intro">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus velit minima accusamus omnis reprehenderit!
+        <b>TP Contact :</b> Coder un formulaire de contact.<br>
+        Deux bouttons en fin : un pour un appel synchrone, un autre pour un appel ajax vers ./form.php . Choisissez les champs de formulaires comme si c'était le contact de votre portfolio.
     </p>
 </div>
 

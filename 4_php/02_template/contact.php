9266c01bd72ae0ef20133240679bf538b4993070
owlab-io <contact@owlab.io>
2017-12-08 07:40:35 +0100

    rebase

--- 4_php/02_template/contact.php
+++ 4_php/02_template/contact.php
@@ -5,7 +5,7 @@
 <div id="app" class="page">
     <h1 class="title"><?php echo $config->title_contact ?></h1>
     <p class="intro">
-        <b>TP Contact :</b> Coder un formulaire de contact.<br>
+        <b>TP Contact Form:</b> Coder un formulaire de contact.<br>
         Deux bouttons en fin : un pour un appel synchrone, un autre pour un appel ajax vers ./form.php . Choisissez les champs de formulaires comme si c'était le contact de votre portfolio.
     </p>
 </div>

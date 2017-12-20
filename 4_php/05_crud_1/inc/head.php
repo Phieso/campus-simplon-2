65040269005a4fde72e17b0f5c3a9b928c68c40d
guillaumeamg <guillaumeamg@meta-one.local>
2017-12-20 11:45:46 +0100

    ajout session crud 1

---
+++ 4_php/05_crud_1/inc/head.php
@@ -0,0 +1,14 @@
+<?php include "crud.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="styles.css">
+  <title>CRUD 1</title>
+</head>
+<body>
+  <h1 class="title">
+    <a href="index.php">CRUD 1</a>
+  </h1>

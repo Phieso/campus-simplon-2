a45210889fc287470ec9d0fde4337a5c0f5601a8
Sophie <hs.huynhsophie@gmail.com>
2018-03-02 16:23:31 +0100

    ajout Google maps API

--- 3_js/07_@sarcelles/day-one/app.js
+++ 3_js/07_@sarcelles/day-one/app.js
@@ -26,24 +26,24 @@ const monModule = (function monModule() {
     xhr.send();
   }
 
-  function getCafes()  {
+  function getCafes() {
     const url = `https://opendata.paris.fr/api/records/1.0/search/?dataset=liste-des-cafes-a-un-euro&rows=10000&facet=arrondissement`;
     const main = document.getElementById("main_content");
 
     function getData(res) {
-     const cafes = JSON.parse(res).records; // tous les cafés
-     const list = document.createElement("ul");
-     list.classList.add("list-cafes");
+      const cafes = JSON.parse(res).records; // tous les cafés
+      const list = document.createElement("ul");
+      list.classList.add("list-cafes");
 
-     cafes.forEach(function(cafe) {
-       // console.log(cafe); // chaque café
-       const li = document.createElement("li");
-       li.className = "item";
-       li.textContent = cafe.fields.nom_du_cafe + " - " + cafe.fields.arrondissement;
-       list.appendChild(li);
-     });
+      cafes.forEach(function (cafe) {
+        // console.log(cafe); // chaque café
+        const li = document.createElement("li");
+        li.className = "item";
+        li.textContent = cafe.fields.nom_du_cafe + " - " + cafe.fields.arrondissement;
+        list.appendChild(li);
+      });
 
-     main.appendChild(list);
+      main.appendChild(list);
     }
 
     makeAJAXCall(getData, url, "GET");
@@ -59,19 +59,19 @@ const monModule = (function monModule() {
     makeAJAXCall(process, `pages/${ref}.html`, "GET");
   }
 
-  function handleClickHeader()  {
+  function handleClickHeader() {
     console.log("le click fonctionne, yata !!!");
   }
 
   function handleClickItems(e) {
-      const src = e.target || e.srcElement; // on teste pour compatibilité cross-browser...
-      // console.log(this); // this ici est l'élément source de l'event !
-      // console.log(this === src); // affiche true
-      styleItem(src);
-      loadPageContent(src.getAttribute("data-page"));
+    const src = e.target || e.srcElement; // on teste pour compatibilité cross-browser...
+    // console.log(this); // this ici est l'élément source de l'event !
+    // console.log(this === src); // affiche true
+    styleItem(src);
+    loadPageContent(src.getAttribute("data-page"));
   }
 
-  function styleItem(item)  {
+  function styleItem(item) {
     const prev = document.querySelector("#nav_main .item.is-active");
 
     if (prev && prev !== item) {
@@ -119,3 +119,107 @@ console.log(monModule.getMaVarPublique());
 // (function foo() {
 //   console.log("bar");
 // }());
+var map;
+var infowindow;
+var i;
+
+function initMap() {
+  var simplon_montreuil = { lat: 48.854521, lng: 2.435999 };
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: simplon_montreuil,
+    zoom: 18
+    // draggable: false
+  });
+
+  var marker = new google.maps.Marker({
+    position: simplon_montreuil,
+    map: map,
+    title: 'Simplon.co'
+  });
+
+  var contentString = '<div id="content">' +
+    '<div id="siteNotice">' +
+    '</div>' +
+    '<h1 id="firstHeading" class="firstHeading">Simplon.co</h1>' +
+    '<div id="bodyContent">' +
+    '<p>55 Rue de Vincennes </br> 93100 Montreuil </p>' +
+    '<p>Site web : <a href="https://simplon.co/">https://simplon.co/</p>' +
+    '</div>' +
+    '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  marker.addListener('mouseover', function () {
+    infowindow.open(map, marker);
+  });
+
+  marker.addListener('mouseout', function () {
+    infowindow.close(map, marker);
+  });
+
+  createMarkers();
+
+}
+
+function createMarkers() {
+  
+  var markers = [
+    {name: 'A.P.E.I Papillons Blancs Vincennes', latlong:{lat:48.854333, lng:2.435880}, adresse: '59 Rue de Vincennes <br/> 93100 Montreuil' }, 
+    {name: 'Daquin et Ferriere Architecture', latlong:{lat:48.854432, lng:2.435358}, adresse: '68 Rue de Vincennes <br/>93100 Montreuil'}, 
+    {name: 'Abitbol Victor', latlong:{lat:48.854241, lng:2.435540}, adresse: '72 Rue de Vincennes, <br/>93100 Montreuil'}];
+
+  // for (i = 0; i < markers.length; i++) {
+    
+  //   var newMarkers = new google.maps.Marker({
+  //     map: map,
+  //     title: markers[i]["name"],
+  //     position: markers[i]["latlong"]
+  //   });
+  // }
+  var markgeo = [];
+
+  markers.forEach( elmt => {
+    var newMarkers = new google.maps.Marker({
+      map: map,
+      title: elmt["name"],
+      position: elmt["latlong"]
+    });
+
+    markgeo.push(elmt["latlong"]);
+
+      var newContentString = '<div id="content">' +
+    '<div id="siteNotice">' +
+    '</div>' +
+    '<h1 id="firstHeading" class="firstHeading">'+ elmt["name"] + '</h1>' +
+    '<div id="bodyContent">' +
+    '<p>'+ elmt["adresse"] +' </p>' +
+    '</div>' +
+    '</div>';
+
+  var newInfowindow = new google.maps.InfoWindow({
+    content: newContentString
+  });
+
+  newMarkers.addListener('mouseover', function () {
+    newInfowindow.open(map, newMarkers);
+  });
+
+  newMarkers.addListener('mouseout', function () {
+    newInfowindow.close(map, newMarkers);
+  });
+
+})
+
+  var flightPath = new google.maps.Polyline({
+    path: markgeo,
+    geodesic: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+  });
+
+  flightPath.setMap(map);
+
+}
